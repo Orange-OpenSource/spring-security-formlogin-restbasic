e16158ba52222b18720d1170cd27b9b604604329
Christophe Maldivi <christophe.maldivi@orange.com>
2016-06-10 22:39:21 +0200

    chore(deploy): build an executable war to prep openshift deployment

--- .openshift/action_hooks/build.sh
+++ .openshift/action_hooks/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 export JAVA_HOME=/etc/alternatives/java_sdk_1.8.0
+export M2_HOME=$OPENSHIFT_DATA_DIR/apache-maven-3.3.9
+export PATH=$M2_HOME/bin:$PATH
 export PATH=$JAVA_HOME/bin:$PATH
 
 cd $OPENSHIFT_REPO_DIR
-mvn package -DskipTests=true
+mvn -s $OPENSHIFT_DATA_DIR/m2/settings.xml package -DskipTests=true
+
--- .openshift/action_hooks/start.sh
+++ .openshift/action_hooks/start.sh
@@ -4,4 +4,4 @@ export JAVA_HOME=/etc/alternatives/java_sdk_1.8.0
 export PATH=$JAVA_HOME/bin:$PATH
 
 cd $OPENSHIFT_REPO_DIR
-nohup java -jar target/*.jar --server.port=${OPENSHIFT_DIY_PORT} --server.address=${OPENSHIFT_DIY_IP} &
+nohup java -jar target/*.war --server.port=${OPENSHIFT_DIY_PORT} --server.address=${OPENSHIFT_DIY_IP} &
