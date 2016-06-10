d4c7fffb1de4528d3f24c6fd18b825c8f2dc06f3
Christophe Maldivi <christophe.maldivi@orange.com>
2016-06-10 23:02:55 +0200

    chore(deploy): build an executable war to prep openshift deployment

--- .openshift/action_hooks/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME=/etc/alternatives/java_sdk_1.8.0
-export PATH=$JAVA_HOME/bin:$PATH
-
-cd $OPENSHIFT_REPO_DIR
-nohup java -jar target/*.war --server.port=${OPENSHIFT_DIY_PORT} --server.address=${OPENSHIFT_DIY_IP} &
